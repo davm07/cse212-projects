73da0fd2cc53f99beb8a97897209bc9c626dcf42
Diego Vasquez <122320757+davm07@users.noreply.github.com>
2024-12-10 00:20:56 +0100

    Finish 4 problems

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,33 +13,49 @@ public class Node
     {
         // TODO Start Problem 1
 
-        if (value < Data)
+        if (value != Data)
         {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+
+        return value < Data
+            ? Left != null && Left.Contains(value)
+            : Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (this == null)
+        {
+            return 0;
+        }
+
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
